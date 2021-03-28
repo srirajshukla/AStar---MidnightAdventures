583b321458f06c39898f85718cd244adafaf6809
srirajshukla <srirajshukla@gmail.com>
2021-03-29 01:25:57 +0530

    Added GetNeighbours
    GetNeighbours returns a list of node containg all the neighbours of a given node.

--- Assets/Scenes/Grid.cs
+++ Assets/Scenes/Grid.cs
@@ -30,10 +30,44 @@ public class Grid : MonoBehaviour
             {
                 Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y*nodeDiameter + nodeRadius);
                 bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
-                grid[x, y] = new Node(walkable, worldPoint);
+                grid[x, y] = new Node(walkable, worldPoint, x, y);
             }
         }
     }
+
+    /// <summary>
+    /// This method returns the neighbours of any node in the grid. 
+    /// </summary>
+    /// <param name="node">
+    ///     The node whose neighbours are to be found. 
+    /// </param>
+    /// <returns>
+    /// Returns a list of nodes that are neighbours of the given node.  
+    /// </returns>
+    public List<Node> GetNeighbours(Node node)
+    {
+        List<Node> neighbours = new List<Node>();
+
+        for(int x=-1; x<=1; x++)
+        {
+            for(int y=-1; y<=1; y++)
+            {
+                if (x == 0 && y == 0)
+                    continue;
+
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                if (checkX >=0 && checkX <gridSizeX && checkY >=0 && checkY < gridSizeY)
+                {
+                    neighbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+
+        return neighbours;
+    }
+
 
     public Node NodeFromWorldPoint(Vector3 worldPosition)
     {
@@ -50,6 +84,7 @@ public class Grid : MonoBehaviour
         
     }
 
+    public List<Node> path;
     void OnDrawGizmos()
     {
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
@@ -60,6 +95,11 @@ public class Grid : MonoBehaviour
             foreach (Node n in grid)
             {
                 Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                if (path != null)
+                {
+                    if (path.Contains(n))
+                        Gizmos.color = Color.black;
+                }
                 Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.01f));
             }
         }
