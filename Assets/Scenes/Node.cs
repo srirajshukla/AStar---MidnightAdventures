36614dce5a7441deeae85aa2f6378999bb4e398d
srirajshukla <srirajshukla@gmail.com>
2021-03-29 00:30:25 +0530

    A single node in the scene.

---
+++ Assets/Scenes/Node.cs
@@ -0,0 +1,16 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public bool walkable;
+
+    public Vector3 worldPosition;
+
+    public Node(bool walkable_param, Vector3 worldPosition_param)
+    {
+        walkable = walkable_param;
+        worldPosition = worldPosition_param;
+    }
+}
