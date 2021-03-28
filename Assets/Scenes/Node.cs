4bf08a74fcc77c05128f7c2e7e465c10e9d6d4c4
srirajshukla <srirajshukla@gmail.com>
2021-03-29 01:25:03 +0530

    Parameters for A* - gCost, hCost, fCost and Parent to keep track where we came from.
    Added methods for gcost, hcost and fcost of a node. Also added a parent for node. A parent node is the node from which we have travelled to this node.

--- Assets/Scenes/Node.cs
+++ Assets/Scenes/Node.cs
@@ -5,12 +5,32 @@ using UnityEngine;
 public class Node
 {
     public bool walkable;
-
     public Vector3 worldPosition;
+    public int gridX;
+    public int gridY;
 
-    public Node(bool walkable_param, Vector3 worldPosition_param)
+    public int gCost;   // gCost is the cost from starting location.
+    public int hCost;   // hCost is the cost from final location.
+
+    public Node parent;
+
+
+    public Node(bool walkable_param, Vector3 worldPosition_param, int gridX_param, int gridY_param)
     {
         walkable = walkable_param;
         worldPosition = worldPosition_param;
+        gridX = gridX_param;
+        gridY = gridY_param;
+    }
+
+    /// <summary>
+    /// fCost is the sum of gCost and hCost.
+    /// </summary>
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
     }
 }
